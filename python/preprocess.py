043f6e854df9ed8a09a8aa9ef1d8cac63923da4c
Clinton Pahl <crpahl@Clintons-MacBook-Pro.local>
2012-11-25 03:50:16 -0700

    Added bag-of-words preprocessing and the generated bag-of-words csv files

--- python/preprocess.py
+++ python/preprocess.py
@@ -1,26 +1,28 @@
+""" NOTE: the pyenchant package is required to run this script
+"""
 import csv
 import string
 import enchant
 
+from termDocumentMatrix import TermDocumentMatrix
 from enchant.checker import SpellChecker
 from enchant.tokenize import get_tokenizer, HTMLChunker, EmailFilter, URLFilter, WikiWordFilter
 
-DATA_FILES = ['test_with_solutions.csv',
-              'train.csv']
+TEST = 'test_with_solutions.csv'
+TRAIN = 'train.csv'
 
 dictionary = enchant.Dict("en_US")
 tokenizer = get_tokenizer(tag="en_US", chunkers=[HTMLChunker], filters=[EmailFilter, URLFilter, WikiWordFilter])
 
-def preprocess():
+def preprocessSpellCheck():
     """ Parses the comments from each csv file in DATA_FILES and creates a new
         correpsonding file with comments that have been delimited into words
         that have been spell checked.
     """
     for dataFile in DATA_FILES:
-        inputFile = None
         try:
             inputFile = open("../data/" + dataFile, 'rb')
-            outputFile = open("../data/processed_" + dataFile, 'wb')
+            outputFile = open("../data/spellChecked_" + dataFile, 'wb')
             fileReader = csv.reader(inputFile, delimiter=',')
             fileWriter = csv.writer(outputFile, delimiter=',')
             fileReader.next() #Skip header labels
@@ -33,12 +35,61 @@ def preprocess():
             inputFile.close()
             outputFile.close()
 
+def preprocessBagOfWords():
+    """ Creates a new bag-of-words csv for each csv in DATA_FILES. Each entry in the
+        bag-of-words csv represents the number of times the corresponding word occurs
+        in the comment.
+
+        NOTE: The list of words each comment is compared against is compiled from every
+        word in the test set. From what I've read so far, selecting useful features,
+        or words in our case, can be tricky. Some things to consider are:
+
+            - Ignore stop words (you, a, if, etc...)
+            - Don't include words that occur rarely -- for example, a word only seen
+              in a single comment
+            - Consider different methods of tokenizing words -- for example, should we also
+              include numbers, grammar, internet words (memes), etc...
+            - How to accurately replace words that are mispelled. Should we always take the
+              first suggestion or should we have some sort of heuristic?
+    """
+    addWords = True
+    tdm = TermDocumentMatrix()
+
+    #TRAIN must be processed first in order to construct bag-of-words properly
+    for dataFile in [TRAIN, TEST]:
+        try:
+            inputFile = open("../data/" + dataFile, 'rb')
+            outputFile = open("../data/Y_" + dataFile, 'wb')
+            fileReader = csv.reader(inputFile, delimiter=',')
+            fileWriter = csv.writer(outputFile, delimiter=',')
+            fileReader.next() #Skip header labels
+
+            if dataFile == TEST:
+                tdm.clear_docs()
+                addWords = False
+
+            for row in fileReader:
+                comment = row[2]
+                insult = row[0]
+                words = tokenizeAndSpellCheck(comment)
+                tdm.add_doc(words, addWordsToDictionary=addWords)
+                fileWriter.writerow(insult)
+        finally:
+            tdm.write_csv("../data/X_bagOfWords_" + dataFile, cutoff=1)
+            inputFile.close()
+            outputFile.close()
+
 def tokenizeAndSpellCheck(comment):
     """ Tokenizes the comment into words and spell checks each word with pyenchant.
+
+        NOTE: Some major work may need to be done here to insure we're getting the
+        best features (words)
     """
     l = []
     for word in tokenizer(comment):
         suggestions = dictionary.suggest(word[0])
+        #Remove suggestions that have a space (multiple words). Also consider removing hyphenated words.
+        suggestions = filter(lambda x:' ' not in x, suggestions)
         if suggestions:
             l.append(suggestions[0])
     
---
+++ python/termDocumentMatrix.py
@@ -0,0 +1,78 @@
+""" 
+A slighty modified version of the TermDocumentMatrix from the textmining package.
+"""
+
+
+import csv
+
+class TermDocumentMatrix:
+
+    """
+    Class to efficiently create a term-document matrix.
+
+    The only initialization parameter is a tokenizer function, which should
+    take in a single string representing a document and return a list of
+    strings representing the tokens in the document. If the tokenizer
+    parameter is omitted it defaults to using textmining.simple_tokenize
+
+    Use the add_doc method to add a document (document is a string). Use the
+    write_csv method to output the current term-document matrix to a csv
+    file. You can use the rows method to return the rows of the matrix if
+    you wish to access the individual elements without writing directly to a
+    file.
+
+    """
+
+    def __init__(self):
+        # The term document matrix is a sparse matrix represented as a
+        # list of dictionaries. Each dictionary contains the word
+        # counts for a document.
+        self.sparse = []
+        # Keep track of the number of documents containing the word.
+        self.doc_count = {}
+
+    def add_doc(self, words, addWordsToDictionary=True):
+        """Add document to the term-document matrix."""
+        # Count word frequencies in this document
+        word_counts = {}
+        for word in words:
+            word_counts[word] = word_counts.get(word, 0) + 1
+        # Add word counts as new row to sparse matrix
+        self.sparse.append(word_counts)
+
+        if addWordsToDictionary:
+            # Add to total document count for each word
+            for word in word_counts:
+                self.doc_count[word] = self.doc_count.get(word, 0) + 1
+
+    def clear_docs(self):
+        """Clear all currently added docs but retain the dictionary"""
+        self.sparse = []
+
+    def rows(self, cutoff=2):
+        """Helper function that returns rows of term-document matrix."""
+        # Get master list of words that meet or exceed the cutoff frequency
+        words = [word for word in self.doc_count \
+          if self.doc_count[word] >= cutoff]
+        # Return header
+        yield words
+        # Loop over rows
+        for row in self.sparse:
+            # Get word counts for all words in master list. If a word does
+            # not appear in this document it gets a count of 0.
+            data = [row.get(word, 0) for word in words]
+            yield data
+
+    def write_csv(self, filename, cutoff=2):
+        """
+        Write term-document matrix to a CSV file.
+
+        filename is the name of the output file (e.g. 'mymatrix.csv').
+        cutoff is an integer that specifies only words which appear in
+        'cutoff' or more documents should be written out as columns in
+        the matrix.
+
+        """
+        f = csv.writer(open(filename, 'wb'))
+        for row in self.rows(cutoff=cutoff):
+            f.writerow(row)
