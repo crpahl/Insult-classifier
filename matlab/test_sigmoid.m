50842ca4c810f29acb22bde54f9975ef91e67203
James Osgood <jamesosogood@gmail.com>
2012-11-28 15:57:19 -0700

    Added confidence interval and better error reporting.

--- matlab/test_sigmoid.m
+++ matlab/test_sigmoid.m
@@ -2,29 +2,37 @@
 clear all
 load ../data/data.mat;
 
-tic
-
 trainNum = 50;
 testNum = 100;
 mXtrain = Xtrain(1:trainNum, :);
 mytrain = ytrain(1:trainNum);
 
+tic
+
 % Classification -> Strong Probability
 for i = 1:trainNum
     if ytrain(i) == 0
-        mytrain(i) = ytrain(i) + 0.00001;
+        mytrain(i) = ytrain(i) + 1e-6;
     else % ytrain(i) == 1
-        mytrain(i) = ytrain(i) - 0.00001;
+        mytrain(i) = ytrain(i) - 1e-6;
     end
 end
 
 [model] = train_kernel_sigmoid(mXtrain, mytrain, 0.5, 'bow_kernel', 'min');
 yhat = pred_kernel_sigmoid(Xtest(1:testNum, :), model); 
 
-err = abs(yhat - ytest(1:testNum))/length(yhat);
+% Probability -> Classification (95% confidence)
+for i = 1:testNum
+    if yhat(i) >= 0.95
+        yhat(i) = 1;
+    else 
+        yhat(i) = 0;
+    end
+end
 
 timespent(1) = toc;
-timespent
 
-err
-totalErr = sum(err) / testNum
+err = (yhat ~= ytest(1:testNum))
+totalAvgErr = sum(err) / testNum
+
+timespent
