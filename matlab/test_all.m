c5f98c014126ab333ee20bf7f1650d233fb8cfb4
James-Osgood <jamesosogood@gmail.com>
2012-12-11 21:43:02 -0700

    Will test everything (almost).

---
+++ matlab/test_all.m
@@ -0,0 +1,17 @@
+% tests all sets of algorithms with bootstrap_test('...', 200, 400, 4)
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('lse', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('soft', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('hard', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('sigmoid', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('lin', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('gauss', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('min', 200, 400, 4);
+
+[errors, bestLearner, results, loss, testTime] = bootstrap_test('binary', 200, 400, 4);
